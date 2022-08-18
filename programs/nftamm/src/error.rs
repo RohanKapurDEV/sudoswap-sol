d419d217c9f9d3382f8871163702a58ea3138f04
RohanXKapur <rohankapurdev@outlook.com>
2022-08-19 07:16:31 +0800

    add error mod

---
+++ programs/nftamm/src/error.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ProgramError {
+    #[msg("Metadata account does not exist")]
+    MetadataDoesntExist,
+    #[msg("Edition account does not exist")]
+    EditionDoesntExist,
+    #[msg("Derived key invalid")]
+    DerivedKeyInvalid,
+    #[msg("Invalid pair type")]
+    InvalidPairType,
+    #[msg("Invalid bonding curve")]
+    InvalidBondingCurve,
+    #[msg("Invalid delta")]
+    MetadataDeserializeError,
+    #[msg("Invalid collection")]
+    InvalidCollection,
+    #[msg("Invalid collection mints")]
+    InvalidCollectionMint,
+    #[msg("Invalid collection details")]
+    InvalidCollectionDetails,
+    #[msg("Invalid funding amount")]
+    InvalidFundingAmount,
+    #[msg("Invalid owner")]
+    InvalidOwner,
+    #[msg("Invalid quote token mint")]
+    InvalidQuoteTokenMint,
+    #[msg("Nft is not part of a verified collection")]
+    NftNotVerified,
+    #[msg("Pair is not active")]
+    PairNotActive,
+    #[msg("fee param must be between 0 and 10000")]
+    InvalidFee,
+    #[msg("Fee too large")]
+    FeeTooLarge,
+}
